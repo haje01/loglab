cc3f0a278c63c4205ca6e3f370cd4401c2bf74d0
JeongJu Kim <haje01@gmail.com>
2021-09-03 17:46:57 +0900

    test

--- loglab/util.py
+++ loglab/util.py
@@ -1,6 +1,8 @@
 """유틸리티 모음."""
 import os
 from pathlib import Path
+from urllib.request import urlopen
+from urllib.parse import urlparse
 import json
 
 from jsonschema import validate
@@ -8,19 +10,39 @@ from jsonschema import validate
 LOGLAB_HOME = Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute()
 
 
-def verify_labfile(labfile):
-    sfile = os.path.join(LOGLAB_HOME, "schema/lab.schema.json")
-    with open(sfile, 'rt') as f:
-        schema = f.read()
-        schema = json.loads(schema)
+def verify_labfile(lab_path, scm_path=None):
+    """랩파일을 검증.
 
-    with open(labfile, 'rt') as f:
-        body = f.read()
-        body = json.loads(body)
+    Args:
+        lab_path (str): 랩파일 URI
+        scm_path (str): 스키마 파일 URI
 
-    validate(body, schema=schema)
+    """
+    if scm_path is None:
+        scm_path = os.path.join(LOGLAB_HOME, 'schema/lab.schema.json')
+    schema = load_file_from(scm_path)
+    schema = json.loads(schema)
+    lab = load_file_from(lab_path)
+    lab = json.loads(lab)
+
+    validate(lab, schema=schema)
 
 
-if __name__ == '__main__':
-    # 테스트용
-    verify_labfile(os.path.join(LOGLAB_HOME, "schema/test.lab.json"))
+def load_file_from(path):
+    """지정된 로컬 또는 웹에서 텍스트 파일 읽기.
+
+    Args:
+        path (str): 로컬 파일 경로 또는 URI
+
+    Returns:
+        str: 읽어들인 파일 내용
+    """
+    parsed = urlparse(path)
+    if parsed.scheme in ('http', 'https'):
+        # 웹 파일
+        with urlopen(path) as f:
+            return f.read()
+    else:
+        # 로컬 파일
+        with open(path, 'rt') as f:
+            return f.read()
