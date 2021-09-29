829e803cc73e8364b05b7b96a194d66264d21bec
JeongJu, Kim <haje01@gmail.com>
2021-09-29 16:01:29 +0900

    onefile

--- tools/build.bat
+++ tools/build.bat
@@ -4,7 +4,7 @@ set ver=%%F
 )
 set version=%ver:~9,5%
 echo -- Build version %version% --
-pyinstaller loglab\cli.py --name loglab --icon image/loglab.ico --noconfirm --add-data="schema/lab.schema.json;schema"
+pyinstaller loglab\cli.py --name loglab --icon image/loglab.ico --onefile --noconfirm --add-data="schema/lab.schema.json;schema"
 set wver="%version%.0"
 rem pyinstaller loglab.spec --name loglab --icon image/loglab.ico
 tools\verpatch.exe dist\loglab\loglab.exe %wver% /va /pv %wver% /s description "LogLab - Design & Validate Log Files." /s product "LogLab"
