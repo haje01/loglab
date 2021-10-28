3c7d851c39524a1c638b6b0a7f0ad951988669ff
JeongJu, Kim <haje01@gmail.com>
2021-10-28 13:54:51 +0900

    binary copyright

--- tools/build.bat
+++ tools/build.bat
@@ -9,4 +9,4 @@ echo -- Build version %version% --
 pyinstaller loglab\cli.py --name loglab --icon image/loglab.ico --onefile --noconfirm --add-data="schema/lab.schema.json;schema" | rem
 timeout 3
 set wver="%version%.0"
-tools\verpatch.exe dist\loglab.exe %wver% /va /pv %wver% /s description "Design & Validate Log Files." /s product "LogLab"
+tools\verpatch.exe dist\loglab.exe %wver% /va /pv %wver% /s description "Design & Validate Log Files." /s product "LogLab" /s copyright "(c) 2021"
