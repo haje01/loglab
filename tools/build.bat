7bed60667bcd6de1cb4fd0be2d598a248eaa8338
JeongJu, Kim <haje01@gmail.com>
2021-11-26 16:54:25 +0900

    fix binary data file

--- tools/build.bat
+++ tools/build.bat
@@ -6,7 +6,12 @@ set ver=%%F
 )
 set version=%ver:~9,5%
 echo -- Build version %version% --
-pyinstaller loglab\cli.py --name loglab --icon image/loglab.ico --onefile --noconfirm --add-data="schema/lab.schema.json;schema" | rem
+pyinstaller loglab\cli.py --name loglab --icon image/loglab.ico --onefile --noconfirm ^
+    --add-data="schema/lab.schema.json;schema" ^
+    --add-data="template/tmpl_doc.html.jinja;template" ^
+    --add-data="template/tmpl_obj.cs.jinja;template" ^
+    --add-data="template/tmpl_obj.py.jinja;template" ^
+    | rem
 timeout 3
 set wver="%version%.0"
 tools\verpatch.exe dist\loglab.exe %wver% /va /pv %wver% /s description "Design & Validate Log Files." /s product "LogLab" /s copyright "(c) 2021"
