195b47a4664cc0d2d7e526c36a1b8b5b94a39ce9
Jeong Ju, Kim <haje01@webzen.com>
2025-07-25 10:46:39 +0900

    Resolves #49

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ __version__ = version
 
 from setuptools import find_packages, setup
 
-SCRIPTS = ["bin/loglab"]
 if os.name == "nt":
-    SCRIPTS += ["bin/loglab.bat"]
+    SCRIPTS = ["bin/loglab.bat"]
+else:
+    SCRIPTS = ["bin/loglab"]
 
 setup(
     name="loglab",
